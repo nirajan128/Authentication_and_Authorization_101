4ffd0f57695665178e1d4862563e6de2937d5685
Nirajan <nirajanshrestha672@gmail.com>
2025-02-24 17:14:15 -0500

    Project Initiated

---
+++ server/dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const PORT = 5000;
+//Use cors let the frontend URL access of the backend
+app.use((0, cors_1.default)({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+app.use(express_1.default.json());
+app.get("/", (req, res) => {
+    res.json("Helloo");
+});
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+const PORT = 5000;
+
+//Use cors let the frontend URL access of the backend
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
+  
+  app.use(express.json());
+
+  app.get("/", (req,res)=>{
+    res.json("Helloo")
+  })
+
+  // Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
