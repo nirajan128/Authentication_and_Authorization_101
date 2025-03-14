7c5f316618ba97eb681acc990489cc95bde37037
Nirajan <nirajanshrestha672@gmail.com>
2025-03-14 11:51:00 -0400

    [Section 7: Final touches]: Re adjusted styles for Navigation bar and homepage: OAuth

--- OAUTH/client/src/Component/Homepage.tsx
+++ OAUTH/client/src/Component/Homepage.tsx
@@ -2,9 +2,12 @@ import Navigation from "./Navigation";
 
 const Homepage: React.FC = ()=>{
     return (
-        <>
+        <div>
         <Navigation />
-        </>
+        <div className="d-flex justify-content-center align-items-center p-2" style={{height: "80vh"}}>
+      <h1 className="shadow p-3">Google OAUTH</h1>
+      </div>
+        </div>
     )
 }
 
--- OAUTH/client/src/Component/Navigation.tsx
+++ OAUTH/client/src/Component/Navigation.tsx
@@ -2,20 +2,16 @@ import { loginWithGoogle } from "../Utilities/Api";
 function Navigation(){
     return(
         <>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container">
-        <a className="navbar-brand" href="#">
-            <img src="logo.png" alt="Logo" width="40" height="40" className="d-inline-block align-text-top" />
-            MyWebsite
-        </a>
+        <h1 className="text-white">OAUTH</h1>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
             <div className="d-flex">
-            <button className="btn btn-outline-light" type="button" onClick={loginWithGoogle}>Log In WIth google</button>
-            
+            <button className="btn btn-outline-light" type="button" onClick={loginWithGoogle}>Log In</button>
             </div>
         </div>
     </div>
